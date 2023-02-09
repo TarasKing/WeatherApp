33de08528374fc1ec430759d4d2d805e2a5ce7e0
TarasKing <ivanovtaras@live.ru>
2023-02-09 15:33:24 -0800

    Initial Commit

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Taras on 09/02/2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
